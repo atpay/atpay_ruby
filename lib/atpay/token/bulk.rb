a467d1d3529335346c4115dc7c34d9041ded5ac9
James Kassemi <jkassemi@gmail.com>
2014-07-10 15:04:31 -0600

    Adds ability to set custom expiration time, and enables auth-only on both bulk and invoice tokens.

--- lib/atpay/token/bulk.rb
+++ lib/atpay/token/bulk.rb
@@ -7,8 +7,16 @@ module AtPay
         @version = 2
       end
 
+      def expires_in_seconds=(v)
+        @expires_in_seconds = v
+      end
+
+      def expires_in_seconds
+        @expires_in_seconds || (60 * 60 * 24 * 14)
+      end
+
       def to_s
-        AtPay::Token::Encoder.new(session, @version, amount, nil, 60 * 60 * 24 * 14, url, user_data).email
+        AtPay::Token::Encoder.new(session, @version, amount, nil, expires_in_seconds, url, user_data).email
       end
     end
   end
--- lib/atpay/token/invoice.rb
+++ lib/atpay/token/invoice.rb
@@ -1,8 +1,20 @@
 module AtPay
   module Token
     class Invoice < Struct.new(:session, :amount, :email_address, :user_data)
+      def auth_only!
+        @version = 2
+      end
+
+      def expires_in_seconds=(v)
+        @expires_in_seconds = v
+      end
+
+      def expires_in_seconds
+        @expires_in_seconds || (60 * 60 * 24 * 14)
+      end
+
       def to_s
-        Token::Encoder.new(session, nil, amount, email_address, 60 * 60 * 24 * 14, nil, user_data).email
+        Token::Encoder.new(session, @version, amount, email_address, expires_in_seconds, nil, user_data).email
       end
     end
   end
